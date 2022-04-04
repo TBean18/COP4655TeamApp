5c8170f87b6df76914cef7920a789f4c25a2cb3d
Juan <jmarq075@fiu.edu>
2022-04-04 19:37:39 -0400

    xml changes

--- app/src/main/java/com/cop4655/puggymotivation/MainActivity.java
+++ app/src/main/java/com/cop4655/puggymotivation/MainActivity.java
@@ -1,19 +1,53 @@
 package com.cop4655.puggymotivation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.cop4655.puggymotivation.fragments.ProfileFragment;
+import com.cop4655.puggymotivation.fragments.QuotesFragment;
+import com.cop4655.puggymotivation.fragments.StylesFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     //Add code that will allow the user to go from Quotes, Profiles, Styles screens in order to have the app to flow
     //xml code temporary for now
+    public static final String TAG = "MainActivity";
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                Fragment fragment;
+//                switch (menuItem.getItemId()) {
+//                    case R.id.action_quotes:
+//                        fragment = new QuotesFragment();
+//                        break;
+//                    case R.id.action_styles:
+//                        fragment = new StylesFragment();
+//                        break;
+//                    case R.id.action_profile:
+//                    default:
+//                        fragment = new ProfileFragment();
+//                        break;
+//                }
+//                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+//                return true;
+//            }
+//        });
+//        bottomNavigationView.setSelectedItemId(R.id.action_quotes);
 
     }
 
